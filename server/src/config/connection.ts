1954ec60cc6c8339ad24998a60991215dcfca7fe
Ajthompson88 <aj.thompson8888@gmail.com>
2025-05-02 01:01:13 -0400

    connection to the servr setup

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,8 +1,21 @@
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
-import mongoose from 'mongoose';
+const connectionString = process.env.MONGO_DB || '';
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz');
+if (!connectionString) {
+  throw new Error('MONGO_DB environment variable is not defined');
+}
 
-export default mongoose.connection;
+mongoose.connect(connectionString);
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB Atlas');
+});
+
+export default db;
