1cec6cefa4ca42f7cdbd340f6b83099be77879b1
Ajthompson88 <aj.thompson8888@gmail.com>
2025-05-02 10:48:47 -0400

    Changes to the connection file to trace deployment issues

--- server/dist/config/connection.js
+++ server/dist/config/connection.js
@@ -1,7 +1,16 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
-const connectionString = process.env.MONGO_DB || '';
+const uri = process.env.MONGO_DB;
+if (!uri)
+    throw new Error('MONGO_DB environment variable is not defined');
+mongoose.connect(uri, { /* options */})
+    .then(() => console.log('✅ Connected to MongoDB'))
+    .catch(err => {
+    console.error('connection error:', err);
+    process.exit(1);
+});
+const connectionString = process.env.MONGO_DB || 'mongodb://localhost:27017/techquiz';
 if (!connectionString) {
     throw new Error('MONGO_DB environment variable is not defined');
 }
--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,7 +3,16 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const connectionString = process.env.MONGO_DB || '';
+const uri = process.env.MONGO_DB;
+if (!uri) throw new Error('MONGO_DB environment variable is not defined');
+mongoose.connect(uri, { /* options */ })
+  .then(() => console.log('✅ Connected to MongoDB'))
+  .catch(err => {
+    console.error('connection error:', err);
+    process.exit(1);
+  });
+
+const connectionString = process.env.MONGO_DB || 'mongodb://localhost:27017/techquiz';
 
 if (!connectionString) {
   throw new Error('MONGO_DB environment variable is not defined');
@@ -19,3 +28,4 @@ db.once('open', () => {
 });
 
 export default db;
+
