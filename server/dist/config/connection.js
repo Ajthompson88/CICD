94d79a38f1e00ad59c205cab74c3ac697a0d94aa
Andrew Thompson <Aj.thompson8888@gmail.com>
2025-05-02 10:31:19 -0400

    Update connection.js

--- server/dist/config/connection.js
+++ server/dist/config/connection.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
-const connectionString = process.env.MONGO_DB || '';
+const connectionString = process.env.MONGO_DB || 'mongodb://localhost:27017/techquiz';
 if (!connectionString) {
     throw new Error('MONGO_DB environment variable is not defined');
 }
