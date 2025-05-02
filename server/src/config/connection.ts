bed261a49304477016cb02e80bd08749c2312a6b
Andrew Thompson <Aj.thompson8888@gmail.com>
2025-05-02 10:31:37 -0400

    Update connection.ts

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const connectionString = process.env.MONGO_DB || '';
+const connectionString = process.env.MONGO_DB || 'mongodb://localhost:27017/techquiz';
 
 if (!connectionString) {
   throw new Error('MONGO_DB environment variable is not defined');
